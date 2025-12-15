8c4d8c6a381e4ff61b1f228aff87b3211cd763ac
Jaqueline Silva <js654367@gmail.com>
2025-12-15 17:31:38 -0300

    atualizacao

--- src/main/java/br/gov/sus/scvs/dominio/unidades/HospitalPublico.java
+++ src/main/java/br/gov/sus/scvs/dominio/unidades/HospitalPublico.java
@@ -3,9 +3,8 @@ package br.gov.sus.scvs.dominio.unidades;
 public class HospitalPublico extends UnidadeSaude {
     private int leitosDisponiveis;
 
-    public HospitalPublico(String nome, String endereco, String telefone, boolean aberta, int leitosDisponiveis) {
-        super(nome, endereco, telefone, aberta);
-        this.leitosDisponiveis = leitosDisponiveis;
+    public HospitalPublico(String nome) {
+        super(nome);
     }
 
     @Override
